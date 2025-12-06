e858ae726cc1742257889a2c82b439c1b4623983
Matheus Perozini Magalhães <perozinimagalhesmatheus@gmail.com>
2025-12-06 09:54:26 -0300

    Fixed

--- src/modules/locals/application/use-cases/list-locals.usecase.ts
+++ src/modules/locals/application/use-cases/list-locals.usecase.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@nestjs/common';
 import { LocalsEntity } from '../../domain/entities/locals.entity';
 import { ILocalRepository } from '../../domain/interfaces/locals.repository.interface';
 
+@Injectable()
 export class ListLocalsUseCase {
   constructor(private readonly repo: ILocalRepository) {}
 
